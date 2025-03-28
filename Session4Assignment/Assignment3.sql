ec392916e29f262fe46501430ec7a27d8344cbba
Swayam Khatter <swayam.khatter@metacube.com>
2025-03-28 13:00:01 +0530

    ...

--- Session4Assignment/Assignment3.sql
+++ Session4Assignment/Assignment3.sql
@@ -1,10 +1,10 @@
 -- Assignment 3:
 
 -- Index for Orders Table (Indexing OrderDate for faster queries on date filtering)
-CREATE INDEX idx_orders_date ON Orders (timestamp);
+CREATE INDEX idx_orders_date ON Orders (order_id);
 
 -- Index for Products Table (Indexing ProductID for faster joins with OrderDetails)
-CREATE INDEX idx_products_id ON Products (product_iD);
+CREATE INDEX idx_products_id ON Products (product_id);
 
 -- Index for Categories Table (Indexing CategoryID for quicker category-based lookups)
-CREATE INDEX idx_categories_id ON Categories (category_iD);
+CREATE INDEX idx_categories_id ON Categories (category_id);
